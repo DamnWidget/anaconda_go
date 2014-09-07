9b5a5c4beb1783847f9cc7766adb343f5a47df07
Oscar Campos <oscar.campos@member.fsf.org>
2014-09-08 00:55:03 +0100

    added anaconda_lib with go utilities

---
+++ anaconda_lib/go/detector.py
@@ -0,0 +1,126 @@
+
+# Copyright (C) 2014 - Oscar Campos <oscar.campos@member.fsf.org>
+# This program is Free Software see LICENSE file for details
+
+import os
+import time
+import sublime
+import subprocess
+
+from ..anaconda_plugin import create_subprocess
+
+
+class GolangDetector:
+    """Detect the local golang installation (if any)
+    """
+
+    _detected = False
+    GOROOT = None
+    GOPATH = None
+    CGO_ENABLED = None
+
+    def __call__(self):
+        start = time.time()
+        if self._detect_in_environment():
+            self._detected = True
+
+        if not self._detected and self._detect_in_shell():
+            self._detected = True
+
+        if self._detected:
+            print('AnacondaGO: finished detecting environment in {}s'.format(
+                time.time() - start
+            ))
+        else:
+            sublime.error_message(
+                'AnacondaGO has been unable to determine the following '
+                'required environment variables:\n'
+                '\tGOROOT {}\n\tGOPATH {}\n\tCGO_ENABLED {}\n\n'
+                'AnacondaGO can\'t work without those variables, that means '
+                'that the plugin is not going to work out of the box and you '
+                'will provide the correct values for these variables in any '
+                'configuration level (project settings or AnacondaGO settings)'
+                ''.format(self.GOROOT, self.GOPATH, self.CGO_ENABLED)
+            )
+
+    @property
+    def go_binary(self):
+        """Return back the go binary location if Go is detected
+        """
+
+        if is_golang_available():
+            return os.path.join(self.GOROOT, 'bin', 'go')
+
+    def _detect_in_environment(self):
+        """Detect Go parameters in the inheritted shell environemnt vars
+        """
+
+        environ = os.environ
+        self.GOROOT = environ.get('GOROOT')
+        self.GOPATH = environ.get('GOPATH')
+        self.CGO_ENABLED = environ.get('CGO_ENABLED')
+
+        if self.GOROOT is None or self.GOPATH is None:
+            return False
+
+        self._normalize_cgo()
+        return True
+
+    def _detect_in_shell(self):
+        """Detect Go parameters using the shell directly
+        """
+
+        if sublime.platform() == 'windows':
+            return self._detect_in_windows_shell()
+
+        return self._detect_in_unix_shell()
+
+    def _detect_in_windows_shell(self):
+        """Detect Go parameters in windows shell (normally cmd)
+        """
+
+        shell = os.environ.get('COMSPEC', 'cmd')
+        self._spawn_processes(shell)
+        if self.GOROOT is None or self.GOPATH is None:
+            return False
+
+        self._normalize_cgo()
+        return True
+
+    def _detect_in_unix_shell(self):
+        """Deect Go parameters in unix shell (normally sh)
+        """
+
+        shell = os.environ.get('SHELL', 'sh')
+        self._spawn_processes(shell)
+        if self.GOROOT is None or self.GOPATH is None:
+            return False
+
+        self._normalize_cgo()
+        return True
+
+    def _spawn_processes(self, shell):
+        """Spawn a shell process and ask for go environment variables
+        """
+
+        for var in ['GOROOT', 'GOPATH', 'CGO_ENABLED']:
+            args = [shell, '/C', 'go', 'env', var]
+            proc = create_subprocess(*args, stdout=subprocess.PIPE)
+            output, _ = proc.communicate()
+            setattr(self, var, output)
+
+    def _normalize_cgo(self):
+        """Set CGO_ENABLED to False if is not set
+        """
+
+        if self.CGO_ENABLED is None:
+            self.CGO_ENABLED = False
+
+
+init = GolangDetector()
+
+
+def is_golang_available():
+    """Determines if golang is available or not
+    """
+    return init._detected
