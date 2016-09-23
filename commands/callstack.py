199ee1559f23917f8a00cc38fd5e80f1b9435920
Oscar Campos <oscar.campos@member.fsf.org>
2016-09-23 23:28:03 +0100

    added callees, callers and callstack commands

---
+++ commands/callstack.py
@@ -0,0 +1,128 @@
+
+# Copyright (C) 2016 - Oscar Campos <oscar.campos@member.fsf.org>
+# This program is Free Software see LICENSE file for details
+
+import traceback
+
+import sublime
+import sublime_plugin
+
+from anaconda_go.lib import go, panels
+from anaconda_go.lib.plugin import typing
+from anaconda_go.lib.helpers import get_settings, get_scope
+from anaconda_go.lib.plugin import Worker, Callback, JediUsages, is_code
+
+
+class AnacondaGoCallstack(sublime_plugin.TextCommand):
+    """Execute guru and callers any returnd definition
+    """
+
+    def run(self, edit: sublime.Edit) -> None:
+        try:
+            view = self.view
+            scope = get_settings(view, 'anaconda_go_guru_scope')
+            row, col = view.rowcol(view.sel()[0].begin())
+            offset = view.text_point(row, col)
+            code = view.substr(sublime.Region(0, view.size()))
+
+            data = {
+                'vid': view.id(),
+                'scope': scope if scope is not None else get_scope(view, go.GOPATH),  # noqa
+                'path': view.file_name(),
+                'offset': offset,
+                'modified_buffer': self._modified_buffer(view, code),
+                'go_env': {
+                    'GOROOT': go.GOROOT,
+                    'GOPATH': go.GOPATH,
+                    'CGO_ENABLED': go.CGO_ENABLED
+                },
+                'method': 'callstack',
+                'handler': 'anaGonda'
+            }
+            Worker().execute(
+                Callback(
+                    on_success=self._on_success,
+                    on_failure=self._on_failure,
+                    on_timeout=self._on_timeout
+                ),
+                **data
+            )
+        except Exception as err:
+            print('anaconda_go: callers error')
+            print(traceback.print_exc())
+
+    def is_enabled(self) -> bool:
+        """Determine if this command is enabled or not
+        """
+
+        if len(sublime.active_window().views()) == 0:
+            return False
+
+        if not go.ANAGONDA_PRESENT:
+            return False
+
+        return is_code(self.view, lang='go')
+
+    def _on_success(self, data):
+        """Process result and normalize it for anaconda's goto
+        """
+
+        if get_settings(self.view, 'anaconda_go_print_callstack', True):
+            # print the callstack in a doc panel
+            panel = panels.DocPanel(self.view)
+            panel.show()
+            panel.print(self._pretiffy(data))
+
+        JediUsages(self).process(True, self._normalize(data))
+
+    def _on_failure(self, data: typing.Dict) -> None:
+        """Fired on failures from the callback
+        """
+
+        print('anaconda_go: callers error')
+        print(data['error'])
+
+    def _on_timeout(self, data: typing.Dict) -> None:
+        """Fired when the callback times out
+        """
+
+        print('Golang callers definition timed out')
+
+    def _modified_buffer(self, view: sublime.View, code: str) -> str:
+        """Guru needs this to use unsaved buffers instead of files
+        """
+
+        return '\n'.join([
+            view.file_name(), str(len(code.encode('utf8'))), code
+        ])
+
+    def _normalize(self, data: typing.Dict) -> typing.Dict:
+        """Normalize tools output into anaconda's goto format
+        """
+
+        callstack = []
+        for result in data['result'].get('callers', []):
+            path, line, col = result['pos'].split(':')
+            callstack.append((path, int(line), int(col)))
+
+        return {
+            'success': True,
+            'usages': callstack,
+            'vid': data['vid']
+        }
+
+    def _pretiffy(self, data: typing.Dict) -> str:
+        """Pretiffy callstack for print into panel
+        """
+
+        callstack = []
+        callers = data['result'].get('callers', [])
+        for i in range(len(callers)):
+            callstack.append('{0}{3}{1}\n{0}{3}{2}'.format(
+                '\t' * i,
+                callers[i]['caller'],
+                callers[i]['pos'],
+                '|_ ' if i > 0 else ''
+            ))
+
+        return '\n'.join(callstack)
