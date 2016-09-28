f2b1e3615e384c335e83599866cad3aa62ead5f7
Oscar Campos <oscar.campos@member.fsf.org>
2016-09-28 17:48:14 +0100

    prevent cyclical saves when autoformat is active

--- listeners/autoformat.py
+++ listeners/autoformat.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2016 - Oscar Campos <oscar.campos@member.fsf.org>
 # This program is Free Software see LICENSE file for details
 
+import time
 import sublime_plugin
 
 from anaconda_go.lib.plugin import is_code
@@ -12,10 +13,17 @@ class AnacondaGoAutoFormatEventListener(sublime_plugin.EventListener):
     """AnacondaGO goimports formatter event listener class
     """
 
+    _last_save = time.time()
+
     def on_pre_save(self, view: sublime_plugin.sublime.View) -> None:
         """Called just before the file is going to be saved
         """
 
+        if time.time() - self._last_save < 1:
+            return
+
         auto_format = get_settings(view, 'anaconda_go_auto_format', False)
         if auto_format and is_code(view, lang='go'):
             view.run_command('anaconda_go_format')
+
+        self._last_save = time.time()
